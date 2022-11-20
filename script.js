3b6f4c15ee5dbc2f702a2e3fe5a6c903162253d0
Brandon Collins <38891846+collins777@users.noreply.github.com>
2022-11-19 23:54:00 -0500

    updated code to display map marker

--- script.js
+++ script.js
@@ -14,24 +14,39 @@ const inputElevation = document.querySelector('.form__input--elevation');
 // geo-location
 navigator.geolocation.getCurrentPosition(
   function (position) {
-    console.log(position);
-    // get coordinates from position object
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
     const coords = [latitude, longitude];
     const map = L.map('map').setView(coords, 13);
+    //console.log(map);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
+    // get user location on click & place marker
+    map.on('click', function (mapEvent) {
+      console.log(mapEvent);
+      const { lat, lng } = mapEvent.latlng;
+
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+    });
   },
+
   function () {
     alert('Could not get your location');
   }
