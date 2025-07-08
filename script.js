7e5fe719b449b960ce97f966ff835155e9ecf869
Brandon Collins <brandoncollins@Brandons-MBP-2.attlocal.net>
2025-07-08 18:21:23 -0400

    refactor map event handling and form submission for location pins

--- script.js
+++ script.js
@@ -11,7 +11,10 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// get users current location
+// allows map, mapEvent to be used in multiple event scopes
+let map, mapEvent;
+
+// ----------------------------------------------- get users current location
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -24,7 +27,7 @@ if (navigator.geolocation) {
       const coords = [latitude, longitude];
 
       // when browser gets location, update map
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       // display map in browser
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -32,31 +35,56 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      // event listener
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-
-        // add location pin to map, configure popup, set popup content
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Workout')
-          .openPopup();
+      // handle click on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        // toggle the hidden form class on click
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function (error) {
+      // log potential errors to the console
       console.log('Geolocation error code:', error.code);
       console.log('Geolocation error message:', error.message);
       alert('Could not get your location');
     }
   );
 }
+
+// ----------------------------------------------- form event listener
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // clear input fields on submit
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  // display marker on submit
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  // add location pin to map, configure popup, set popup content
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+// listen for change on input type
+inputType.addEventListener('change', function () {
+  // toggle hidden class on closest parent form__row
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
